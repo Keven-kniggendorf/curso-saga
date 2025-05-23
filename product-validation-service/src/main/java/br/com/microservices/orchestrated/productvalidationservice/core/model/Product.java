6c3547ef3f9fe4cb7b8f73b13980d1e68650b976
Keven <kevenkniggendorf@gmail.com>
2025-05-23 16:26:11 -0300

    montando as classes de negócio;

---
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/model/Product.java
@@ -0,0 +1,24 @@
+package br.com.microservices.orchestrated.productvalidationservice.core.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String code;
+
+
+
+}
---
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package br.com.microservices.orchestrated.productvalidationservice.core.repository;
+
+import br.com.microservices.orchestrated.productvalidationservice.core.model.Product;
+import br.com.microservices.orchestrated.productvalidationservice.core.model.Validation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+
+    Boolean existsByCode(String code);
+
+
+
+}
