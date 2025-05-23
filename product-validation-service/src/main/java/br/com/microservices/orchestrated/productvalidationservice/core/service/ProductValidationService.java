c725bbc4874edcd6b34a553da05ca6d549bca46d
Keven <kevenkniggendorf@gmail.com>
2025-05-23 18:06:34 -0300

    Criando validações de campos nulos;

---
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/service/ProductValidationService.java
@@ -0,0 +1,125 @@
+package br.com.microservices.orchestrated.productvalidationservice.core.service;
+
+import br.com.microservices.orchestrated.productvalidationservice.core.dto.Event;
+import br.com.microservices.orchestrated.productvalidationservice.core.dto.History;
+import br.com.microservices.orchestrated.productvalidationservice.core.dto.OrderProducts;
+import br.com.microservices.orchestrated.productvalidationservice.core.enums.ESagaStatus;
+import br.com.microservices.orchestrated.productvalidationservice.core.model.Validation;
+import br.com.microservices.orchestrated.productvalidationservice.core.producer.KafkaProducer;
+import br.com.microservices.orchestrated.productvalidationservice.core.repository.ProductRepository;
+import br.com.microservices.orchestrated.productvalidationservice.core.repository.ValidationRepository;
+import br.com.microservices.orchestrated.productvalidationservice.core.utils.JsonUtil;
+import jakarta.validation.ValidationException;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class ProductValidationService {
+
+    private static final String CURRENT_SOURCE = "PRODUCT_VALIDATION_SERVICE";
+
+    private final JsonUtil jsonUtil;
+    private final KafkaProducer kafkaProducer;
+    private final ProductRepository productRepository;
+    private final ValidationRepository validationRepository;
+
+
+    public void validateExistingProducts(Event event) {
+
+        try {
+            checkCurrentValidation(event);
+            createValidation(event, true);
+            handleSuccess(event);
+        } catch (Exception e) {
+            log.error("Error trying to validate product: ", e);
+            handleFailCurrentExecuted(event, e.getMessage());
+        }
+        kafkaProducer.sendEvent(jsonUtil.toJson(event));
+    }
+
+
+    private void validateProductsInformed(Event event) {
+        if (ObjectUtils.isEmpty(event.getPayload()) || ObjectUtils.isEmpty(event.getPayload().getProducts())) {
+            throw new ValidationException("Product list is empty!");
+
+        }
+        if (ObjectUtils.isEmpty(event.getPayload().getId()) || ObjectUtils.isEmpty(event.getPayload().getTransactionId())) {
+            throw new ValidationException("OrderID and TransactionID must be informed!");
+
+        }
+
+    }
+
+
+    private void checkCurrentValidation(Event event) {
+        validateProductsInformed(event);
+        if (validationRepository.existsByOrderIdAndTransactionId(
+                event.getOrderId(), event.getTransactionId())) {
+            throw new ValidationException("OrderID and/or TransactionID exists!");
+        }
+        event.getPayload().getProducts().forEach(product -> {
+            validateProductInformed(product);
+            validateExistingProduct(product.getProduct().getCode());
+        });
+
+    }
+
+    private void validateProductInformed(OrderProducts product) {
+
+        if (ObjectUtils.isEmpty(product.getProduct()) || ObjectUtils.isEmpty(product.getProduct().getCode())) {
+            throw new ValidationException("Product code is empty!");
+        }
+
+    }
+
+
+    private void validateExistingProduct(String code) {
+        if (!productRepository.existsByCode(code)) {
+            throw new ValidationException("Product code does not exist!");
+        }
+
+    }
+
+    private void createValidation(Event event, boolean success ) {
+
+        var validation = Validation.builder()
+                .orderId(event.getPayload().getId())
+                .transactionId(event.getTransactionId())
+                .success(success)
+                .build();
+        validationRepository.save(validation);
+
+
+    }
+
+
+    private void handleSuccess(Event event) {
+        event.setStatus(ESagaStatus.SUCCESS);
+        event.setSource(CURRENT_SOURCE);
+        addHistory(event, "Products are validated successfully!");
+    }
+
+    private void addHistory(Event event, String message){
+        var history = History
+                .builder()
+                .source(event.getSource())
+                .status(event.getStatus())
+                .message(message)
+                .createdAt(LocalDateTime.now())
+                .build();
+            event.addHistory(history);
+    }
+
+
+
+
+
+
+
+}
